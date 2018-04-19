87d25742c9c8d0afc60fa6a191643d4c883c1f0c
Henrique <henrique@mustorze.com>
2018-04-19 07:14:13 -0300

    Change filter locate

--- src/Traits/Filterable.php
+++ src/Traits/Filterable.php
@@ -9,57 +9,28 @@ namespace Mustorze\MustAFilter\Traits;
 trait Filterable
 {
     /**
-     * @var null Await for filter instanced class
-     */
-    private $filter = null;
-
-    /**
-     * Filterable constructor.
-     * @throws \Exception
-     */
-    public function __construct()
-    {
-        if (!isset($this->filterClass)) {
-            throw new \Exception('filterClass private var do not found in ' . self::class);
-        }
-
-        $this->filter = new $this->filterClass;
-    }
-
-    /**
      * Generate the data for GraphQL Args
      *
+     * @param $filter
      * @param $data
      * @return mixed
-     * @throws \Exception
      */
-    public static function getFilterArgs($data)
+    public static function getFilterArgs($filter, $data)
     {
-        return self::getFilter()->getFilterArgs($data);
-    }
-
-    /**
-     * Get instanced Filter class
-     *
-     * @return mixed
-     * @throws \Exception
-     */
-    public static function getFilter()
-    {
-        $class = self::class;
-        return (new $class)->filter;
+        return (new $filter)->getFilterArgs($data);
     }
 
     /**
      * Apply the filters in current builder
      *
      * @param $builder
+     * @param $filter
      * @param bool $graphQL
      * @param array $args
      * @return mixed
      */
-    public function scopeFilter($builder, $graphQL = false, array $args = [])
+    public function scopeFilter($builder, $filter, $graphQL = false, array $args = [])
     {
-        return $this->filter->apply($builder, $graphQL, $args);
+        return (new $filter)->apply($builder, $graphQL, $args);
     }
 }
